b6b3fc5d1e5dd8edcc7928e211b8ff407eea6cf6
ValentinoBellini <valentinobellini.dev@gmail.com>
2025-05-15 12:21:35 +0200

    milestone 2

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const products = [
     { name: 'Mela', price: 0.5 },
     { name: 'Pane', price: 1.2 },
@@ -9,6 +11,17 @@ const products = [
 
 
 export default function ShoppingList() {
+
+
+    const [addedProducts, setAddedProducts] = useState([]);
+
+    function addToCart(product) {
+        const alreadyInCart = addedProducts.some((p) => p.name === product.name)
+        if (!alreadyInCart) {
+            setAddedProducts([...addedProducts, { ...product, quantity: 1 }])
+        }
+    }
+
 
     return (
         <>
@@ -26,9 +39,44 @@ export default function ShoppingList() {
                                     <span className="price">prezzo: </span>
                                     {product.price.toFixed(2)} €
                                 </p>
+                                <button
+                                    onClick={() => addToCart(product)}
+
+                                >
+                                    Aggiungi al carrello
+                                </button>
                             </li>)
                     })}
                 </ul>
+
+                {addedProducts.length > 0 && (
+                    <>
+                        <div className="cart">
+                            <h2>Carrello della spesa</h2>
+                            <ul >
+                                {addedProducts.map((product, i) => {
+                                    return (
+                                        <li key={i}>
+                                            <p>
+                                                <span className="name">prodotto: </span>
+                                                {product.name}
+                                            </p>
+                                            <p>
+                                                <span className="price">prezzo: </span>
+                                                {product.price.toFixed(2)} €
+                                            </p>
+                                            <p>
+                                                <span className="name">quantità: </span>
+                                                {product.quantity}
+                                            </p>
+
+                                        </li>)
+                                })}
+                            </ul>
+                        </div>
+                    </>
+                )}
+
             </div>
         </>
     )
